94466b1633d88b98b0c69dbd4800fefc6c1466a7
yassine abderrahmani <yassine04.97@hotmail.fr>
2019-12-11 16:21:46 +0100

    Ajout d'un SplashScreen. Modification de la couleur de fond. Activité principal vide pour l'instant.

---
+++ app/src/main/java/com/example/appnews/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.example.appnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Appel de l'activité principal depuis notre Splashcreen
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
