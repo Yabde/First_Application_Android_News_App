e5158a462c3ffd10e90dae7c7f6325c08ecdb048
yassine abderrahmani <yassine04.97@hotmail.fr>
2019-12-12 19:23:21 +0100

    correction de certaines choses ...

--- app/src/main/java/com/example/appnews/MainActivity.java
+++ app/src/main/java/com/example/appnews/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.appnews;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    AlertDialog.Builder pas_de_connexion;
+
+    String source = "https://newsapi.org/v2/sources?apiKey=d31f5fa5f03443dd8a1b9e3fde92ec34&language=fr";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+        pas_de_connexion = new AlertDialog.Builder(this);
+        pas_de_connexion.setTitle("Erreur de connexion");
+        pas_de_connexion.setMessage("Connectez vous à internet");  **/
+
     }
 }
--- app/src/main/java/com/example/appnews/SplashScreen.java
+++ app/src/main/java/com/example/appnews/SplashScreen.java
@@ -3,13 +3,16 @@ package com.example.appnews;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         //Appel de l'activité principal depuis notre Splashcreen
 
