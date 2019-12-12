f4ce732f02b4ac8bf68f925edc7bb4a329548194
yassine abderrahmani <yassine04.97@hotmail.fr>
2019-12-12 01:42:20 +0100

    Ajout d'un layout pour prendre en compte le design de ce à quoi va ressembler la vision des News

---
+++ app/src/main/java/com/example/appnews/model/Articles.java
@@ -0,0 +1,108 @@
+package com.example.appnews.model;
+
+public class Articles {
+
+    private String id;
+    private String name;
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
+
+
+    /** Contructeurs **/
+
+    public Articles() {
+    }
+
+    public Articles(String id, String name, String author, String title, String description, String url, String urlToImage, String publishedAt, String content) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.content = content;
+    }
+
+    /** Partie Getters **/
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    /** Partie SETTERS **/
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ app/src/main/java/com/example/appnews/model/Source.java
@@ -0,0 +1,88 @@
+package com.example.appnews.model;
+
+public class Source {
+
+    private String id;
+    private String name;
+    private String description;
+    private String url;
+    private String category;
+    private String language;
+    private String country;
+
+
+    /** Contructeurs **/
+
+    public Source() {
+    }
+
+    public Source(String id, String name, String description, String url, String category, String language, String country) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.category = category;
+        this.language = language;
+        this.country = country;
+    }
+
+    /** Partie Getters **/
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    /** Partie SETTERS **/
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
